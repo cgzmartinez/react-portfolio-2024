9e3a36110fddea392eb650b4be349b31292d3397
cgzmartinez <105771374+cgzmartinez@users.noreply.github.com>
2023-12-18 11:23:01 -0600

    Edited files and added resume

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,16 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      'sans': [
+        'Roboto'
+      ],
+      'serif': [
+        'Roboto Serif'
+      ],
+    },
+    extend: {
+    },
   },
   plugins: [],
 }
