0bb9a7e4f03593f3b0c6612de2d00002f0b9198f
cgzmartinez <105771374+cgzmartinez@users.noreply.github.com>
2023-12-19 14:27:16 -0600

    Added routing to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Navbar from './components/navbar'
+import Work from './components/work'
+import About from './components/about'
+import Contact from './components/contact'
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Navbar />
-    </>
+      <div>
+        <Routes>
+          <Route path='/' element={<Work />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/contact' element={<Contact />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   )
 }
 
