986da78d9031d4eade55d922fdde9f2e2daa2be1
cgzmartinez <105771374+cgzmartinez@users.noreply.github.com>
2023-12-21 11:28:47 -0600

    Updated work.jsx
    Reworked to ensure that it's responsive

--- src/components/work.jsx
+++ src/components/work.jsx
@@ -1,43 +1,22 @@
 import ProfilePic from "../assets/profile-pic.svg?react";
 
-
 const Work = () => {
   return (
-    <div>
-      <div className="flex items-center pt-20">
-        <div className="grid grid-rows-3 grid-flow-col pl-20 ml-20 mt-20">
-          <h1 className="font-serif font-bold text-5xl">Hi! My name is Carlos Martinez</h1>
-          <p className="row-start-2 py-4 font-serif font-normal text-4xl pr-20" >I’m a <span>full-stack web developer</span> with a background in graphic design and education.</p>
-          <p className="row-start-3 py-4 font-serif font-normal text-4xl pr-20" >Currently focused on creating accessible and visually appealing products.</p>
-        </div >
-        <div className="flex pr-20 px-20 mr-20 mt-20 float-right">
-          <ProfilePic className="bounce" width="500" height="588" />
-        </div>
+    <section className="grid gap-8 p-20 md:grid-cols-2 lg:items-center lg:text-left">
+      <div>
+        <h1 className="text-4xl font-serif font-bold pb-6">Hi! My name is Carlos Martinez</h1>
+        <p className="text-xl font-serif font-medium pb-6" >I’m a <span>full-stack web developer</span> with a background in graphic design and education.</p>
+        <p className="text-xl font-serif font-medium pb-6" >Currently focused on creating accessible and visually appealing products.</p>
       </div>
 
-      <div className="px-20 pt-20 ml-20 mt-20">
-        <h1 className="font-serif font-bold text-6xl pt-20">Projects I have worked on</h1>
+      <div>
+        <ProfilePic className="bounce w-full items-end pb-6" height="450" />
       </div>
-      {/* <div className="min-h-screen items-center px-20">
-        <div className="wrap grid grid-cols-2 lg:grid-cols-2 sm:grid-cols-1 md:grid-cols-1 gap-10 py-20">
-          <div className="bounce rectangle flex">
-          </div>
-          <div className="bounce rectangle flex">
-          </div>
-          <div className="bounce rectangle flex">
-          </div>
-          <div className="bounce rectangle flex">
-          </div>
-          <div className="bounce rectangle flex">
-          </div>
-          <div className="bounce rectangle flex">
-          </div>
-        </div>
+
+      <div>
+        <h1 className="text-4xl font-serif font-bold mb-2">Projects I have worked on</h1>
       </div>
-      */}
-
-    </div>
-
+    </section>
   )
 }
 
