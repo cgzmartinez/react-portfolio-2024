086bc5432f743d97b6ad6b9158cfa64b0e5459ed
cgzmartinez <105771374+cgzmartinez@users.noreply.github.com>
2023-12-22 23:32:34 -0600

    Update projectcard.jsx
    Added placeholder for 6 total projects

--- src/components/projectcard.jsx
+++ src/components/projectcard.jsx
@@ -25,6 +25,18 @@ const ProjectCards = () => {
       image:
         "https://images.pexels.com/photos/4164418/pexels-photo-4164418.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
     },
+    {
+      text: "title",
+      subtitle: "subtitle",
+      image:
+        "https://images.pexels.com/photos/4164418/pexels-photo-4164418.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    },
+    {
+      text: "title",
+      subtitle: "subtitle",
+      image:
+        "https://images.pexels.com/photos/4164418/pexels-photo-4164418.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    },
   ];
 
   return (
