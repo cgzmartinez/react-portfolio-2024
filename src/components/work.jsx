eeddbd9801c9573192b31bd9e4e4371cd7202e23
cgzmartinez <105771374+cgzmartinez@users.noreply.github.com>
2023-12-22 23:29:36 -0600

    Update work.jsx
    imported project cards and cleaned up breakpoints

--- src/components/work.jsx
+++ src/components/work.jsx
@@ -1,22 +1,28 @@
 import ProfilePic from "../assets/profile-pic.svg?react";
+import ProjectCards from "./projectcard";
 
 const Work = () => {
   return (
-    <section className="grid gap-8 p-20 md:grid-cols-2 lg:items-center lg:text-left">
-      <div>
-        <h1 className="text-4xl font-serif font-bold pb-6">Hi! My name is Carlos Martinez</h1>
-        <p className="text-xl font-serif font-medium pb-6" >I’m a <span>full-stack web developer</span> with a background in graphic design and education.</p>
-        <p className="text-xl font-serif font-medium pb-6" >Currently focused on creating accessible and visually appealing products.</p>
-      </div>
+    <div>
+      <section className="grid gap-8 m-20 md:grid-cols-2 md:pl-20 md:items-center md:text-left">
+        <div>
+          <h1 className="text-4xl font-serif font-bold pb-6">Hi! My name is Carlos Martinez</h1>
+          <p className="text-xl font-serif font-medium pb-6" >I’m a <span>full-stack web developer</span> with a background in graphic design and education.</p>
+          <p className="text-xl font-serif font-medium pb-6" >Currently focused on creating accessible and visually appealing products.</p>
+        </div>
 
-      <div>
-        <ProfilePic className="bounce w-full items-end pb-6" height="450" />
-      </div>
+        <div>
+          <ProfilePic className="bounce w-full items-end" height="450" />
+        </div>
 
-      <div>
-        <h1 className="text-4xl font-serif font-bold mb-2">Projects I have worked on</h1>
+        <div className="pt-10">
+          <h1 className="text-4xl font-serif font-bold">Projects I have worked on</h1>
+        </div>
+      </section>
+      <div className="p-10">
+        <ProjectCards />
       </div>
-    </section>
+    </div>
   )
 }
 
