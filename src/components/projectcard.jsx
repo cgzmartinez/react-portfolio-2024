fe961d727b2607c4036efd24174f33ca5856ef86
cgzmartinez <105771374+cgzmartinez@users.noreply.github.com>
2023-12-22 23:29:13 -0600

    Created projectcard.jsx
    Created a component for the project cards. Mapped placeholder images and text on cards

---
+++ src/components/projectcard.jsx
@@ -0,0 +1,54 @@
+const ProjectCards = () => {
+
+  const cardItem = [
+    {
+      text: "title",
+      subtitle: "subtitle",
+      image:
+        "https://images.pexels.com/photos/4164418/pexels-photo-4164418.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    },
+    {
+      text: "title",
+      subtitle: "subtitle",
+      image:
+        "https://images.pexels.com/photos/4164418/pexels-photo-4164418.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    },
+    {
+      text: "title",
+      subtitle: "subtitle",
+      image:
+        "https://images.pexels.com/photos/4164418/pexels-photo-4164418.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    },
+    {
+      text: "title",
+      subtitle: "subtitle",
+      image:
+        "https://images.pexels.com/photos/4164418/pexels-photo-4164418.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    },
+  ];
+
+  return (
+    <div className="max-w-[1640px] mx-auto grid md:grid-cols-3 gap-6">
+
+      {cardItem.map(({ text, subtitle, image }, index) => {
+        return (
+          <div key={index} className="bounce rounded-xl relative">
+
+            <div className="absolute w-full h-full bg-black/50 rounded-xl text-white">
+              <p className="font-bold text-2xl pl-4 pt-4">{text} </p>
+              <p className="pl-4">{subtitle}</p>
+            </div>
+
+            <img
+              className="max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl"
+              src={image}
+              alt="/"
+            />
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default ProjectCards;
