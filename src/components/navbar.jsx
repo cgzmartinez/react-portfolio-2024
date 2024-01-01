45ab7348ed0466927a3cdaeae94271efe0e160b1
cgzmartinez <105771374+cgzmartinez@users.noreply.github.com>
2023-12-31 19:27:30 -0600

    Update navbar.jsx
    Adding responsive design for mobile

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,5 +1,6 @@
 import { NavLink } from "react-router-dom";
 import Palette from "../assets/palette-icon.svg?react";
+import { FaBars } from "react-icons/fa6";
 
 const Navbar = () => {
 
@@ -9,7 +10,11 @@ const Navbar = () => {
       <div className="justify-start font-normal text-4xl md:text-5xl">
         <p>cm.</p>
       </div>
-      <div>
+
+      <button>
+        <FaBars className="flex md:hidden justify-end items-center h-7 w-7" />
+      </button>
+      <div className="hidden md:block">
         <ul className="flex justify-end items-center">
           <li className="font-light px-7 text-xl">
             <NavLink to='/' className={({ isActive }) => (isActive ? 'active' : '')}
