ba9bae214a8682f1845d9230d85b87766a9af93d
cgzmartinez <105771374+cgzmartinez@users.noreply.github.com>
2024-01-02 15:42:37 -0600

    Updated work.jsx
    Superficial edits on titles and paragraphs

--- src/components/work.jsx
+++ src/components/work.jsx
@@ -6,9 +6,9 @@ const Work = () => {
     <div>
       <section className="grid gap-8 m-20 md:grid-cols-2 md:pl-20 md:items-center md:text-left">
         <div>
-          <h1 className="text-4xl font-serif font-bold pb-6">Hi! My name is Carlos Martinez</h1>
-          <p className="text-xl font-serif font-medium pb-6" >I’m a <span>full-stack web developer</span> with a background in graphic design and education.</p>
-          <p className="text-xl font-serif font-medium pb-6" >Currently focused on creating accessible and visually appealing products.</p>
+          <h1 className="text-3xl md:text-4xl font-serif font-bold pb-6">Hi! My name is Carlos Martinez</h1>
+          <p className="text-lg md:text-xl font-serif font-medium pb-6" >I’m a <span>full-stack web developer</span> with a background in graphic design and education.</p>
+          <p className="text-lg md:text-xl font-serif font-medium" >Currently focused on creating accessible and visually appealing products.</p>
         </div>
 
         <div>
@@ -16,7 +16,7 @@ const Work = () => {
         </div>
 
         <div className="pt-10">
-          <h1 className="text-4xl font-serif font-bold">Projects I have worked on</h1>
+          <h1 className="text-4xl font-serif font-bold">Projects I’ve Worked On</h1>
         </div>
       </section>
       <div className="px-10">
