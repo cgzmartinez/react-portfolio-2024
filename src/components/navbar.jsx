fbc87c02d77ae715b09fcbf1a378a41a4257577c
cgzmartinez <105771374+cgzmartinez@users.noreply.github.com>
2024-01-02 15:43:35 -0600

    Update navbar.jsx
    Reworked sidebar menu to include working icons

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
 import { NavLink } from "react-router-dom";
 import Palette from "../assets/palette-icon.svg?react";
-import { FaBars, FaXmark } from "react-icons/fa6";
+import { FaBars, FaGithub, FaLinkedin, FaXmark } from "react-icons/fa6";
+import { SiUpwork } from "react-icons/si";
 
 const Navbar = () => {
 
@@ -16,8 +17,9 @@ const Navbar = () => {
   return (
 
     <nav className="flex items-center justify-between p-8">
+      {/*Main Logo*/}
       <div className="justify-start font-normal text-4xl md:text-5xl">
-        <p>cm.</p>
+        <a href="/"><p>cm.</p></a>
       </div>
 
       {/*Toggle Navbar's Mobile Menu*/}
@@ -29,13 +31,14 @@ const Navbar = () => {
       <ul
         className={
           nav
-            ? 'fixed md:hidden left-0 top-0 w-[70%] h-full bg-white rounded-3xl drop-shadow-xl ease-in-out duration-500'
+            ? 'fixed z-40 md:hidden left-0 top-0 w-[70%] h-full bg-white rounded-r-3xl drop-shadow-2xl ease-in-out duration-500'
             : 'ease-in-out w-[70%] duration-500 fixed top-0 bottom-0 left-[-100%]'
         }
       >
+
         {/* Mobile Navigation Items */}
         <div className="justify-start font-normal text-4xl md:text-5xl p-8">
-          <p>cm.</p>
+          <a href="/"><p>cm.</p></a>
         </div>
         <div onClick={handleNav} className="p-8">
           <li className="font-light pb-10 text-xl">
@@ -51,21 +54,31 @@ const Navbar = () => {
             >Contact</NavLink>
           </li>
         </div>
-
+        <div className="grid grid-cols-4 absolute inset-x-0 bottom-0 p-6">
+          <a href="https://github.com/cgzmartinez" target="_blank" rel="noreferrer">
+            <FaGithub size={30} />
+          </a>
+          <a href="https://www.linkedin.com/in/carlos-g-martinez/" target="_blank" rel="noreferrer">
+            <FaLinkedin size={30} />
+          </a>
+          <a href="https://www.upwork.com/freelancers/~0181d2b492c6b37a9e" target="_blank" rel="noreferrer" >
+            <SiUpwork size={30} />
+          </a>
+        </div>
       </ul>
 
       {/*Desktop Navbar*/}
-      <div className="hidden md:block">
+      <div className="hidden md:block items-center">
         <ul className="flex justify-end items-center">
-          <li className="font-light px-7 text-xl">
+          <li className="font-light px-7 text-lg">
             <NavLink to='/' className={({ isActive }) => (isActive ? 'active' : '')}
             >Work</NavLink>
           </li>
-          <li className="font-light px-7 text-xl">
+          <li className="font-light px-7 text-lg">
             <NavLink to='/about' className={({ isActive }) => (isActive ? 'active' : '')}
             >About</NavLink>
           </li>
-          <li className="font-light px-7 text-xl">
+          <li className="font-light px-7 text-lg">
             <NavLink to='/contact' className={({ isActive }) => (isActive ? 'active' : '')}
             >Contact</NavLink>
           </li>
@@ -74,6 +87,9 @@ const Navbar = () => {
           </div>
         </ul>
       </div>
+
+      {/*Clicking off of the menu closes menu*/}
+      <div onClick={handleNav} className="fixed h-[100%] w-[100%] z-30"></div>
     </nav>
   );
 };
