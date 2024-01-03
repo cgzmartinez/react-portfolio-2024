09b134e490645000ec62d55e542489b371ee8750
cgzmartinez <105771374+cgzmartinez@users.noreply.github.com>
2024-01-02 23:58:57 -0600

    Removed "Player" from assets and footer.jsx

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,16 +1,11 @@
-import Player from "../assets/player.svg?react";
-
 const Footer = () => {
   return (
     <div>
-      <div className="grid grid-cols-2">
-        <div className="flex px-7">
-          <Player className="player" />
-        </div>
+      <div className="grid md:grid-cols-2 gap-10 m-10 md:p-20 md:pl-20">
 
-        <div className="float-right pl-20 pt-10 ml-20">
-          <h1 className="font-serif font-bold text-2xl">Find me here</h1>
-          <ul className="font-serif font-medium text-2xl pt-10">
+        <div className="md:text-left pl-10 md:pt-6 md:pl-6">
+          <h1 className="font-serif font-bold text-lg md:text-xl">Find me here</h1>
+          <ul className="font-serif font-medium text-sm md:text-lg pt-10">
             <li><a href="https://github.com/cgzmartinez" target="_blank" rel="noreferrer">GitHub</a></li>
             <li><a href="https://www.linkedin.com/in/carlos-g-martinez/" target="_blank" rel="noreferrer">Linkedin</a></li>
             <li><a href="https://medium.com/@cgzmarti" target="_blank" rel="noreferrer">Medium</a></li>
@@ -18,8 +13,8 @@ const Footer = () => {
           </ul>
         </div>
       </div>
-      <div className="flex items-center justify-center pt-10 pb-20 mt-20">
-        <p>Designed and built by <span>Carlos Martinez</span></p>
+      <div className="flex items-center justify-center py-10">
+        <p className="text-xs">Designed and built by <span>Carlos Martinez</span></p>
       </div>
     </div>
   )
