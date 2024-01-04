9a25e98abe806d174ad06d8e14ec9a1f362dbd8f
cgzmartinez <105771374+cgzmartinez@users.noreply.github.com>
2024-01-03 23:54:57 -0600

    Update footer.jsx
    Decided to make "Spotify player" using Tailwind CSS and React-Icons

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,11 +1,16 @@
+import { FaSpotify } from "react-icons/fa6";
 const Footer = () => {
   return (
     <div>
-      <div className="grid md:grid-cols-2 gap-10 m-10 md:p-20 md:pl-20">
-
-        <div className="md:text-left pl-10 md:pt-6 md:pl-6">
+      <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 gap-8 p-10 md:p-20 md:px-20">
+        <div className="md:pt-6 md:pl-5 md:ml-20">
+          <div className="flex pl-5 bg-[#E9EEE8] h-[85px] w-[full] md:w-[auto] rounded-[50px] items-center">
+            <FaSpotify className="fill-[#59C9A5]" size={45} />
+          </div>
+        </div>
+        <div className="pt-10 pl-5 md:text-left md:pt-6 md:pl-20">
           <h1 className="font-serif font-bold text-lg md:text-xl">Find me here</h1>
-          <ul className="font-serif font-medium text-sm md:text-lg pt-10">
+          <ul className=" font-serif font-medium text-sm md:text-lg pt-10">
             <li><a href="https://github.com/cgzmartinez" target="_blank" rel="noreferrer">GitHub</a></li>
             <li><a href="https://www.linkedin.com/in/carlos-g-martinez/" target="_blank" rel="noreferrer">Linkedin</a></li>
             <li><a href="https://medium.com/@cgzmarti" target="_blank" rel="noreferrer">Medium</a></li>
