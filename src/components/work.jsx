d9043c1f4ff09fa5c513358f429b43cacb1366f6
cgzmartinez <105771374+cgzmartinez@users.noreply.github.com>
2024-01-03 23:55:58 -0600

    Update work.jsx

--- src/components/work.jsx
+++ src/components/work.jsx
@@ -4,9 +4,9 @@ import ProjectCards from "./projectcard";
 const Work = () => {
   return (
     <div>
-      <section className="grid grid-cols-1 gap-8 m-20 md:grid-cols-2 md:pl-20 md:items-center md:text-left">
+      <section className="grid grid-cols-1 gap-8 m-10 md:grid-cols-2 md:pl-20 md:items-center md:text-left">
         <div>
-          <h1 className="text-3xl md:text-4xl font-serif font-bold pb-6">Hi! My name is Carlos Martinez</h1>
+          <h1 className="text-4xl font-serif font-bold pb-6">Hi! My name is Carlos Martinez</h1>
           <p className="text-lg md:text-xl font-serif font-medium pb-6" >I’m a <span>full-stack web developer</span> with a background in graphic design and education.</p>
           <p className="text-lg md:text-xl font-serif font-medium" >Currently focused on creating accessible and visually appealing products.</p>
         </div>
@@ -16,7 +16,7 @@ const Work = () => {
         </div>
 
         <div className="pt-10">
-          <h1 className="text-4xl font-serif font-bold">Projects I’ve Worked On</h1>
+          <h1 className="text-3xl md:text-4xl font-serif font-bold">Projects I’ve Worked On</h1>
         </div>
       </section>
       <div className="px-10">
