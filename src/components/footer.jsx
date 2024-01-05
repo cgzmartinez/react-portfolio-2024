80f25ece847acd94e12bf781cf9c7d336c7d2033
cgzmartinez <105771374+cgzmartinez@users.noreply.github.com>
2024-01-05 14:49:22 -0600

    Update footer.jsx
    Linked listed footer items

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -11,15 +11,15 @@ const Footer = () => {
         <div className="pt-10 pl-5 md:text-left md:pt-6 md:pl-20">
           <h1 className="font-serif font-bold text-lg md:text-xl">Find me here</h1>
           <ul className=" font-serif font-medium text-sm md:text-lg pt-10">
-            <li><a href="https://github.com/cgzmartinez" target="_blank" rel="noreferrer">GitHub</a></li>
-            <li><a href="https://www.linkedin.com/in/carlos-g-martinez/" target="_blank" rel="noreferrer">Linkedin</a></li>
-            <li><a href="https://medium.com/@cgzmarti" target="_blank" rel="noreferrer">Medium</a></li>
-            <li><a href="https://www.upwork.com/freelancers/~0181d2b492c6b37a9e" target="_blank" rel="noreferrer">Upwork</a></li>
+            <a href="https://github.com/cgzmartinez" target="_blank" rel="noreferrer"><li>GitHub</li></a>
+            <a href="https://www.linkedin.com/in/carlos-g-martinez/" target="_blank" rel="noreferrer"><li>Linkedin</li></a>
+            <a href="https://medium.com/@cgzmarti" target="_blank" rel="noreferrer"><li>Medium</li></a>
+            <a href="https://www.upwork.com/freelancers/~0181d2b492c6b37a9e" target="_blank" rel="noreferrer"><li>Upwork</li></a>
           </ul>
         </div>
       </div>
       <div className="flex items-center justify-center py-10">
-        <p className="text-xs">Designed and built by <span>Carlos Martinez</span></p>
+        <p className="text-xs">Designed and built by <a href="https://github.com/cgzmartinez" target="_blank" rel="noreferrer"><span>Carlos Martinez</span></a></p>
       </div>
     </div>
   )
