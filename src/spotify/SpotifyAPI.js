3c741b6e4cd41500e39b960e87da10a5370e3b38
cgzmartinez <105771374+cgzmartinez@users.noreply.github.com>
2024-01-07 23:19:04 -0600

    Created Spotify component
    Created a Spotify file that has the SpotifyAPI.js and the JSX component file

---
+++ src/spotify/SpotifyAPI.js
@@ -0,0 +1,60 @@
+import querystring from "query-string";
+
+const client_id = import.meta.REACT_APP_CLIENT_ID;
+const client_secret = import.meta.REACT_APP_CLIENT_SECRET;
+const refresh_token = import.meta.REACT_APP_REFRESH_TOKEN;
+
+const basic = btoa(`${client_id}:${client_secret}`);
+const NOW_PLAYING_ENDPOINT = "https://api.spotify.com/v1/me/player/currently-playing";
+const TOKEN_ENDPOINT = "https://accounts.spotify.com/api/token";
+
+
+const getAccessToken = async () => {
+  const response = await fetch(TOKEN_ENDPOINT, {
+    method: 'POST',
+    headers: {
+      Authorization: `Basic ${basic}`,
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: querystring.stringify({
+      grant_type: 'refresh_token',
+      refresh_token,
+    }),
+  });
+
+  return response.json();
+};
+
+export const getNowPlaying = async () => {
+  const { access_token } = await getAccessToken();
+
+  return fetch(NOW_PLAYING_ENDPOINT, {
+    headers: {
+      Authorization: `Bearer ${access_token}`
+    }
+  });
+};
+
+export default async function getNowPlayingItem(
+  client_id,
+  client_secret,
+  refresh_token
+) {
+  const response = await getNowPlaying(client_id, client_secret, refresh_token);
+  if (response.status === 204 || response.status > 400) {
+    return false;
+  } const song = await response.json();
+  const albumImageUrl = song.item.album.images[0].url;
+  const artist = song.item.artists.map((_artist) => _artist.name).join(", ");
+  const isPlaying = song.is_playing;
+  const songUrl = song.item.external_urls.spotify;
+  const title = song.item.name;
+
+  return {
+    albumImageUrl,
+    artist,
+    isPlaying,
+    songUrl,
+    title,
+  };
+}
