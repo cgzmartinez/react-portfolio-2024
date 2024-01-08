f6c09f9e2d7722b6b1f7f794248ced07cb32c38d
cgzmartinez <105771374+cgzmartinez@users.noreply.github.com>
2024-01-08 12:18:18 -0600

    Update projectcard.jsx
    Fixed mapping structure to include react-icons and set up the structure for the remaining objects

--- src/components/projectcard.jsx
+++ src/components/projectcard.jsx
@@ -2,57 +2,97 @@ import {
   Card,
   CardHeader,
   CardBody,
-  //CardFooter,
+  CardFooter,
   Typography,
-  //Avatar,
-  //Tooltip,
+
 } from "@material-tailwind/react";
+import { DiMongodb } from "react-icons/di";
+import { FaAngular, FaReact, FaNodeJs } from "react-icons/fa6";
+import { SiExpress, SiTypescript } from "react-icons/si";
+
 
 const ProjectCards = () => {
+
 
   const cardItem = [
     {
       title: "pawed",
       subtitle: "A booking application made with blank and blank. Think airbnb, but for pets.",
       image:
-        "https://images.pexels.com/photos/4164418/pexels-photo-4164418.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+        "https://images.pexels.com/photos/4164418/pexels-photo-4164418.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      icon:
+        <div className="flex">
+          <DiMongodb size={25} />
+          <SiExpress className="mx-1" size={25} />
+          <FaReact size={25} />
+          <FaNodeJs className="mx-1" size={25} />
+        </div>,
+      date: "January 12, 2024"
+
     },
     {
       title: "Schma",
       subtitle: "A calendar planning solution made with blank and blank. Work out hours before you finalize on your calendar.",
       image:
         "https://images.pexels.com/photos/4164418/pexels-photo-4164418.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      icon:
+        <div>
+
+        </div>,
+      date: ""
     },
     {
       title: "Keep",
       subtitle: "A google keep clone made with firebase.",
       image:
         "https://images.pexels.com/photos/4164418/pexels-photo-4164418.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      icon:
+        <div>
+
+        </div>,
+      date: ""
     },
     {
       title: "myFlix Angular App",
       subtitle: "A movie application made with Angular that uses CRUD.",
       image:
         "https://images.pexels.com/photos/4164418/pexels-photo-4164418.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      icon:
+        <div className="flex">
+          <FaAngular size={25} />
+          <SiTypescript className="p-[2px] ml-2" size={25} />
+        </div>,
+      date: ""
+
     },
     {
       title: "Meet App",
       subtitle: "A PWA (Progressive Web Application) made with React that uses a Calendar API to fetch upcoming events.",
       image:
         "https://images.pexels.com/photos/4164418/pexels-photo-4164418.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      icon:
+        <div>
+
+        </div>,
+      date: ""
     },
     {
       title: "Chat App",
       subtitle: "A chat application for mobile devices made using React Native.",
       image:
         "https://images.pexels.com/photos/4164418/pexels-photo-4164418.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      icon:
+        <div>
+
+        </div>,
+      date: ""
     },
   ];
 
   return (
     <div className="max-w-[1640px] mx-auto grid md:grid-cols-2 gap-10 pb-10 md:px-20">
 
-      {cardItem.map(({ title, subtitle, image }, index) => {
+      {cardItem.map(({ title, subtitle, date, image, icon }, index) => {
         return (
           <div key={index} className="bounce hover:drop-shadow-xl relative">
 
@@ -76,29 +116,14 @@ const ProjectCards = () => {
                   {subtitle}
                 </Typography>
               </CardBody>
-              {/* <CardFooter className="flex items-center justify-between">
+              <CardFooter className="flex items-center justify-between">
                 <div className="flex items-center -space-x-3">
-                  <Tooltip content="Natali Craig">
-                    <Avatar
-                      size="sm"
-                      variant="circular"
-                      alt="natali craig"
-                      src="https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1061&q=80"
-                      className="border-2 border-white hover:z-10"
-                    />
-                  </Tooltip>
-                  <Tooltip content="Tania Andrew">
-                    <Avatar
-                      size="sm"
-                      variant="circular"
-                      alt="tania andrew"
-                      src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80"
-                      className="border-2 border-white hover:z-10"
-                    />
-                  </Tooltip>
+
+                  <icon className="">{icon}</icon>
+
                 </div>
-                <Typography className="font-normal">January 10</Typography>
-        </CardFooter> */}
+                <Typography className="font-normal">{date}</Typography>
+              </CardFooter>
             </Card>
           </div>
         );
