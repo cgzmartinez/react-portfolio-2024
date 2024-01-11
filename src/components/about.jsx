c0390408c8a6dceca36fb4d85efd1134504b5f89
cgzmartinez <105771374+cgzmartinez@users.noreply.github.com>
2024-01-11 00:14:43 -0600

    Update about.jsx
    Updated about.jsx with a new about image and updated stylings for the page title

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,14 +1,16 @@
-import ProfilePic from "../assets/profile-pic.svg?react";
+import AboutPic from "../assets/about.svg?react";
 
 const About = () => {
   return (
     <div>
-      <section className="grid gap-8 md:grid-cols-2 md:px-20 md:items-center md:text-left">
-        <div>
-          <ProfilePic className="bounce w-full items-end md:h-[450px] my-10" height="350" />
+      <section className="grid gap-2 md:grid-cols-2 md:px-[80px] md:items-center md:text-left">
+        <h1 className="pt-10 px-20 text-left md:hidden block text-4xl font-serif font-bold"><span>About Me</span></h1>
+
+        <div className="m-10">
+          <AboutPic className="bounce w-full items-end md:h-[500px]" height="450" />
         </div>
         <div className="px-20 md:pt-20 md:px-20 sm:px-20">
-          <h1 className="text-4xl font-serif font-bold pb-10"><span>About Me</span></h1>
+          <h1 className="hidden md:block text-4xl font-serif font-bold pb-10"><span>About Me</span></h1>
           <p className="text-lg font-sans font-regular pb-6" >While initially setting out to work in graphic design, I’ve worked in education since 2019.</p>
           <p className="text-lg font-sans font-regular pb-6" >While working as a teacher, I completed a certification for Full Stack Web Development after building a quiz application for a student’s class. </p>
           <p className="text-lg font-sans font-regular pb-6" >These are some of the technology I’ve been working with recently: </p>
