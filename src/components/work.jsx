946d2f6c55672bcf08bc3e3fba4bcdb2296bc868
cgzmartinez <105771374+cgzmartinez@users.noreply.github.com>
2024-01-11 00:17:07 -0600

    Update work.jsx
    Added a working "resume' button that opens my resume in a new tab and added a LinkedIn button.

--- src/components/work.jsx
+++ src/components/work.jsx
@@ -1,14 +1,25 @@
 import ProfilePic from "../assets/profile-pic.svg?react";
 import ProjectCards from "./projectcard";
+import { FaLinkedin } from "react-icons/fa6";
+import Resume from "../assets/cm-resume-2023.pdf"
 
 const Work = () => {
   return (
     <div>
       <section className="grid grid-cols-1 gap-8 m-10 md:grid-cols-2 md:pl-20 md:items-center md:text-left">
-        <div>
+        <div className="m-6">
           <h1 className="text-4xl font-serif font-bold pb-6">Hi! My name is Carlos Martinez</h1>
           <p className="text-lg md:text-xl font-serif font-medium pb-6" >I’m a <span>full-stack web developer</span> with a background in graphic design and education.</p>
-          <p className="text-lg md:text-xl font-serif font-medium" >Currently focused on creating accessible and visually appealing products.</p>
+          <p className="text-lg md:text-xl font-serif font-medium" >Currently focused on creating accessible and visually appealing products at Grinnell College.</p>
+          <div className="flex items-center">
+            <a href={Resume} target="_blank" rel="noreferrer">
+              <button
+                className="rounded-[10px] bg-[#59C9A5] font-serif p-1 px-2 my-6 font-bold text-lg text-white outline-none"
+              > Resume
+              </button>
+            </a>
+            <FaLinkedin size={40} className="mx-3 fill-[#59C9A5]" />
+          </div>
         </div>
 
         <div>
