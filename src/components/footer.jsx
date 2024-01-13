125aa884c9045de52c17b1e86c5e7933093694ef
cgzmartinez <105771374+cgzmartinez@users.noreply.github.com>
2024-01-12 22:58:32 -0600

    Update footer.jsx
    Experimenting with backdrop-blur

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -4,14 +4,14 @@ const Footer = () => {
     <div>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-8 p-10 md:p-20 md:px-20">
         <div className="md:pt-6 md:pl-5 md:ml-20">
-          <div className="flex pl-5 bg-[#E9EEE8] h-[85px] w-[full] md:w-[auto] rounded-[50px] items-center">
+          <div className="flex pl-5 bg-[#E9EEE8]/70 backdrop-blur-sm h-[85px] w-[full] md:w-[auto] rounded-[50px] items-center outline-none">
             <FaSpotify className="fill-[#59C9A5]" size={45} />
           </div>
         </div>
         <div className="pt-10 text-center md:text-left md:pt-6 md:pl-20">
           <h1 className="font-serif font-bold text-lg md:text-xl">Find me here</h1>
           <footer>
-            <ul className=" font-serif font-medium text-sm md:text-lg pt-6">
+            <ul className="font-serif font-medium text-sm md:text-lg pt-6">
               <a href="https://codepen.io/c_martinez" target="_blank" rel="noreferrer"><li>CodePen</li></a>
               <a href="https://github.com/cgzmartinez" target="_blank" rel="noreferrer"><li>GitHub</li></a>
               <a href="https://www.linkedin.com/in/carlos-g-martinez/" target="_blank" rel="noreferrer"><li>Linkedin</li></a>
