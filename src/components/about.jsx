b95f7af86f0c4bfeb58631deb32b398106e88adf
cgzmartinez <105771374+cgzmartinez@users.noreply.github.com>
2024-01-30 15:24:19 -0600

    Update about.jsx
    Added "card" design for dark mode

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -8,30 +8,33 @@ const About = () => {
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
       transition={{ duration: .5 }}>
-      <section className="grid gap-2 md:grid-cols-2 md:px-[80px] md:items-center md:text-left">
+      <section className="grid gap-2 md:grid-cols-2 md:px-[80px] md:items-center md: md:text-left">
         <h1 className="pt-10 px-20 text-left md:hidden block text-4xl font-serif font-bold dark:text-[#a0c2a4]">About Me</h1>
 
         <motion.div className="m-10" whileHover={{ rotate: 5 }}>
           <AboutPic className="w-full items-end md:h-[500px]" height="450" aria-label="About Picture" />
         </motion.div>
-        <div className="px-20 md:pt-20 md:px-20 sm:px-20">
-          <h1 className="hidden md:block text-4xl font-serif font-bold pb-10 dark:text-[#a0c2a4]">About Me</h1>
-          <p className="text-lg font-sans font-regular pb-6 dark:text-white/50" >While initially setting out to work in graphic design, I’ve worked in education since 2019.</p>
-          <p className="text-lg font-sans font-regular pb-6 dark:text-white/50" >While working as a teacher, I completed a certification for Full Stack Web Development after building a quiz application for a student’s class. </p>
-          <p className="text-lg font-sans font-regular pb-6 dark:text-white/50" >These are some of the technology I’ve been working with recently: </p>
-          <ul className="grid gap-1 grid-cols-3 py-6">
-            <li className="text-sm">JavaScript (ES6+)</li>
-            <li className="text-sm pl-6">React</li>
-            <li className="text-sm">WordPress</li>
+        <div className="px-20 dark:px-10 md:pt-20 md:px-20">
+          <div className="dark:p-10 dark:md:p-10 dark:bg-[#353535] dark:rounded-[25px]">
+            <h1 className="hidden md:block text-4xl font-serif font-bold pb-10 dark:text-[#a0c2a4]">About Me</h1>
+            <p className="text-lg font-sans font-regular pb-6 dark:text-white/50" >While initially setting out to work in graphic design, I’ve worked in education since 2019.</p>
+            <p className="text-lg font-sans font-regular pb-6 dark:text-white/50" >While working as a teacher, I completed a certification for Full Stack Web Development after building a quiz application for a student’s class. </p>
+            <p className="text-lg font-sans font-regular pb-6 dark:text-white/50" >These are some of the technology I’ve been working with recently: </p>
+            
+            <ul className="grid gap-1 grid-cols-3 py-6">
+              <li className="text-sm">JavaScript (ES6+)</li>
+              <li className="text-sm pl-6">React</li>
+              <li className="text-sm">WordPress</li>
 
-            <li className="text-sm">TypeScript</li>
-            <li className="text-sm pl-6">Angular</li>
-            <li className="text-sm">MongoDB</li>
+              <li className="text-sm">TypeScript</li>
+              <li className="text-sm pl-6">Angular</li>
+              <li className="text-sm">MongoDB</li>
 
-            <li className="text-sm">Node.js</li>
-            <li className="text-sm pl-6">Vue</li>
-            <li className="text-sm">Express.js</li>
-          </ul>
+              <li className="text-sm">Node.js</li>
+              <li className="text-sm pl-6">Vue</li>
+              <li className="text-sm">Express.js</li>
+            </ul>
+          </div>
         </div>
 
       </section>
